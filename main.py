a012557ac20f790f417538054fe29cc47e9482f6
dimastpx <dimas30072007@gmail.com>
2024-03-29 19:08:55 +1000

    1.0

---
+++ main.py
@@ -0,0 +1,27 @@
+import qrcode
+import telebot
+from PIL import Image
+from os import remove
+
+
+def savelink(link : str):
+
+    qr = qrcode.make(link)
+    path = "code.png"
+    qr.save(path)
+    file = Image.open(path)
+    return file
+
+
+
+token = "7103218621:AAG0OMIpTCQVir27XZDIclrqIS5my0rv0LI"
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def send(message):
+    bot.send_photo(chat_id=message.chat.id, photo=savelink(message.text))
+    remove("code.png")
+
+bot.polling()
